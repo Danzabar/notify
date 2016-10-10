c332e8e6f9093e92b329f237a688d65b8c3d8a0b
Dan Cox <danzabian@gmail.com>
2016-10-10 08:46:44 +0100

    Adding tests for alert group endpoints

--- alert_test.go
+++ alert_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -13,4 +16,56 @@ func init() {
 
 	Migrate()
 	server = httptest.NewServer(App.router)
+}
+
+func TestPostAlertGroupSuccess(t *testing.T) {
+	payload := []byte(`{"name": "test","type":"mail"}`)
+
+	req, _ := http.NewRequest("POST", fmt.Sprintf("%s/api/v1/alert-group", server.URL), bytes.NewReader(payload))
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var a AlertGroup
+	err = json.NewDecoder(resp.Body).Decode(&a)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, 200, resp.StatusCode)
+	assert.Equal(t, "Test", a.Name)
+	assert.Equal(t, "mail", a.Type)
+}
+
+func TestPostAlertGroupBadJSON(t *testing.T) {
+	payload := byte(`{"test":}`)
+
+	req, _ := http.NewRequest("POST", fmt.Sprintf("%s/api/v1/alert-group", server.URL), bytes.NewReader(payload))
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, 400, resp.StatusCode)
+}
+
+func TestPostAlertGroupValidationError(t *testing.T) {
+	payload := []byte(`{"type": "test"}`)
+
+	req, _ := http.NewRequest("POST", fmt.Sprintf("%s/api/v1/alert-group", server.URL), bytes.NewReader(payload))
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, 400, resp.StatusCode)
+}
+
+func TestPostAlertGroupUnProcessable(t *testing.T) {
+
 }
